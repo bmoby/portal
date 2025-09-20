33e059a125b332c40bc95928c003a14a03668d68
bmoby <msouleymanov@gmail.com>
2025-09-20 12:50:18 +0700

    donation removed

--- src/components/shared/donations/index.ts
+++ src/components/shared/donations/index.ts
@@ -1,4 +1,2 @@
 export { default as StartOverlay } from "./StartOverlay";
 export type { StartOverlayProps } from "./StartOverlay";
-export { default as DonationModal } from "./DonationModal";
-export type { DonationModalProps, DonationBinaryChoice } from "./DonationModal";
